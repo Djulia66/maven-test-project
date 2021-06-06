16aedd9d6a8c2dc473b6272ea14e9e9967add208
Djulia966 <juls96@abv.bg>
2021-06-06 16:51:45 +0300

    init project

---
+++ project/project-4/src/test/java/uni/pu/fmi/st/Test1.java
@@ -0,0 +1,13 @@
+package uni.pu.fmi.st;
+
+import org.junit.Test;
+
+public class Test1 {
+	@Test
+	public void test() throws InterruptedException {
+		System.out.println("---Strat Test1---");
+		Thread.sleep(2500);
+		
+		System.out.println("---Stop Test1---");
+	}
+}
